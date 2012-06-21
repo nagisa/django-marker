ed422b4f1810134886dd7a155c1fac0a40afafcf
Simonas Kazlauskas <simonas@kazlauskas.me>
2012-06-21 18:57:42 +0300

    Make marker use global markdown settings

--- marker/templatetags/settings.py
+++ marker/templatetags/settings.py
@@ -1,14 +1,14 @@
 from django.conf import settings
 
-print('Hello')
-
 _marker = getattr(settings, 'MARKER', {})
+_markdown = _marker.get('markdown', {})
 
 MARKER = {
     'fatal_import': settings.DEBUG and _marker.get('fatal_import', True),
     'markdown': {
-        'html_replace': '<...>',
-        'global_extras': [],
-        'link_patterns': None
-    }
+        'html_replace': _markdown.get('html_replace', '<...>'),
+        'global_extras': _markdown.get('global_extras', []),
+        'link_patterns': _markdown.get('link_patterns', None),
+    },
+
 }
