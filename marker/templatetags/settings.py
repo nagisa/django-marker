0a7058b0e76af2456bdbba126fc143d455b1742a
Simonas Kazlauskas <simonas@kazlauskas.me>
2012-07-04 14:23:16 +0300

    Fix wrong conditional statement

--- marker/templatetags/settings.py
+++ marker/templatetags/settings.py
@@ -4,7 +4,7 @@ _marker = getattr(settings, 'MARKER', {})
 _markdown = _marker.get('markdown', {})
 
 MARKER = {
-    'fatal_import': settings.DEBUG and _marker.get('fatal_import', True),
+    'fatal_import': settings.DEBUG or _marker.get('fatal_import', True),
     'markdown': {
         'global_html': _markdown.get('global_html', []),
         'global_exts': _markdown.get('global_exts', []),
